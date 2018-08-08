f30d482dc9344f4b079503e61f7623d116b1d821
devil199301 <devil199301@gmail.com>
2018-08-08 17:26:28 +0800

    檢查解果輸出txt

--- test/test.js
+++ test/test.js
@@ -85,13 +85,13 @@ const url = "http://localhost:56873/";
 /**
  * 站代號 (目前檢查試玩LOGO用的)
  */
-const webname = 'AB001-01';
+const webname = 'AR012-01';
 /**
- * 首頁內有沒有大廳連結
+ * 首頁大廳連結
  */
-const homebox = true;
+const homeLobbylink = true;
 /**
- * 存圖片404
+ * 404 error
  */
 const errLoad = [];
 /**
@@ -104,7 +104,11 @@ let browser;
 const width = 1400;
 const height = 900;
 
-beforeAll(async () => {
+const getHtmlContent = async() => {
+
+}
+
+beforeAll(async() => {
     browser = await puppeteer.launch({
         headless: false,
         slowMo: 80,
@@ -127,137 +131,139 @@ beforeAll(async () => {
     });
 });
 
-describe("開啟網頁", () => {
-    test("連線", async () => {
+describe("開啟網頁", async() => {
+    test("連線", async() => {
         await page.goto(url);
         const html = await page.content();
-        const $ = cheerio.load(html);
-        const head = $('html').attr('ng-app');
-        expect(head).toBe('portalApp');
-    });
+        // const $ = cheerio.load(html);
+        // const head = $('html').attr('ng-app');
+        // expect(head).toBe('portalApp');
+    }, 15000);
 
-    /** 
-     *  NAV
-     */
-    test("Nav連結", async () => {
-        const html = await page.content();
-        const $ = cheerio.load(html.replace(/\n/g, ''));
-        const liList = $('#nav > ul > li');
-        liList.each((index, element) => {
-            const target = element['attribs']['data-link'];
-            for (let i = 0; i < element.children.length; i++) {
-                if (element.children[i][`type`] === `tag` && element.children[i][`name`] === `a`) {
-                    const link = element.children[i]['attribs']['href'];
-                    if (target === undefined) continue
-                    expect(link).toBe(checkList.link[target]);
-                }
-            }
-        })
-    });
+    // test("Nav連結", async () => {
+    //     const html = await page.content();
+    //     const $ = cheerio.load(html.replace(/\n/g, ''));
+    //     const liList = $('#nav > ul > li');
+    //     liList.each((index, element) => {
+    //         const target = element['attribs']['data-link'];
+    //         for (let i = 0; i < element.children.length; i++) {
+    //             if (element.children[i][`type`] === `tag` && element.children[i][`name`] === `a`) {
+    //                 const link = element.children[i]['attribs']['href'];
+    //                 if (target === undefined) continue
+    //                 expect(link).toBe(checkList.link[target]);
+    //             }
+    //         }
+    //     })
+    // });
 
-    /** 
-     *  下拉
-     */
-    test("下拉", async () => {
-        const html = await page.content();
-        const $ = cheerio.load(html.replace(/\n/g, ''));
-        const liList = $('#nav > ul > li');
-        liList.each((index, element) => {
-            const kind = element['attribs']['data-link'];
-            for (let i = 0; i < element.children.length; i++) {
-                if (element.children[i][`name`] === `ol`) {
-                    for (let j = 0; j < element.children[i].children.length; j++) {
-                        if (element.children[i].children[j][`name`] === `li`) {
-                            const target = element.children[i].children[j].attribs['game-box'];
-                            const link = element.children[i].children[j].attribs['ng-click'];
-                            if (target === undefined) continue
-                            expect(link).toBe(checkList[kind][target]);
-                        }
-                    }
-                }
-            }
-        })
-    });
+    // test("下拉", async () => {
+    //     const html = await page.content();
+    //     const $ = cheerio.load(html.replace(/\n/g, ''));
+    //     const liList = $('#nav > ul > li');
+    //     liList.each((index, element) => {
+    //         const kind = element['attribs']['data-link'];
+    //         for (let i = 0; i < element.children.length; i++) {
+    //             if (element.children[i][`name`] === `ol`) {
+    //                 for (let j = 0; j < element.children[i].children.length; j++) {
+    //                     if (element.children[i].children[j][`name`] === `li`) {
+    //                         const target = element.children[i].children[j].attribs['game-box'];
+    //                         const link = element.children[i].children[j].attribs['ng-click'];
+    //                         if (target === undefined) continue
+    //                         expect(link).toBe(checkList[kind][target]);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     })
+    // });
 
-    if (homebox) {
-        test("首頁大廳連結(GAME-BOX)", async () => {
-            const html = await page.content();
-            const $ = cheerio.load(html.replace(/\n/g, ''));
-            const lobbyList = $('#game-box > li');
-            lobbyList.each((index, element) => {
-                const target = element.attribs['data-img'];
-                for (let i = 0; i < element.children.length; i++) {
-                    if (element.children[i][`type`] === `tag` && element.children[i][`name`] === `a`) {
-                        const link = element.children[i]['attribs']['href'];
-                        expect(link).toBe(checkList.link[target]);
-                    }
-                }
-            })
-        });
-    }
-});
+    // if (homeLobbylink) {
+    //     test("首頁大廳連結(GAME-BOX)", async () => {
+    //         const html = await page.content();
+    //         const $ = cheerio.load(html.replace(/\n/g, ''));
+    //         const lobbyList = $('#game-box > li');
+    //         lobbyList.each((index, element) => {
+    //             const target = element.attribs['data-img'];
+    //             for (let i = 0; i < element.children.length; i++) {
+    //                 if (element.children[i][`type`] === `tag` && element.children[i][`name`] === `a`) {
+    //                     const link = element.children[i]['attribs']['href'];
+    //                     expect(link).toBe(checkList.link[target]);
+    //                 }
+    //             }
+    //         })
+    //     });
+    // }
 
-describe("Lobby", () => {
-    test("Live", async () => {
-        await page.goto(`${url}/Lobby/Live`);
-        const html = await page.content();
-        const $ = cheerio.load(html.replace(/\n/g, ''));
-        const list = $('.game-list > li');
-        list.each((index, element) => {
-            const target = element.attribs['game-box'];
-            const click = element.attribs['ng-click'];
-            expect(click).toBe(checkList.live[target]);
-        });
-    });
+    //TODO: ng-href 檢查 
+    test("a tag", async() => {
+        const response = await page.goto(url);
+        const content = await response.text();
+        const nghref = /ng-href="{{\w*.\w*==''\?\ '#': \w*.\w*}}"/g;
+        const nghrefArray = content.match(nghref);
 
-    test("Sport", async () => {
-        await page.goto(`${url}/Lobby/Sport`);
-        const html = await page.content();
-        const $ = cheerio.load(html.replace(/\n/g, ''));
-        const list = $('.game-list > li');
-        list.each((index, element) => {
-            const target = element.attribs['game-box'];
-            const click = element.attribs['ng-click'];
-            expect(click).toBe(checkList.sport[target]);
-        });
-    });
-
-    test("Lottery", async () => {
-        await page.goto(`${url}/Lobby/Lottery`);
-        const html = await page.content();
-        const $ = cheerio.load(html.replace(/\n/g, ''));
-        const list = $('.game-list > li');
-        list.each((index, element) => {
-            const target = element.attribs['game-box'];
-            const click = element.attribs['ng-click'];
-            expect(click).toBe(checkList.lottery[target]);
-        });
+        
+        console.log(nghrefArray);
     });
 });
 
-/** 
- *  檢查試玩logo路徑
- */
-describe("試玩", () => {
-    test("Logo", async () => {
-        await page.goto(`${url}/Trial/Apply`);
-        const logo = await page.evaluate(() => {
-            const title = document.querySelector('#trial-logo');
-            return getComputedStyle(title)[`background-image`];
-        });
-        const checkUrl = logo.search(webname);
-        expect(checkUrl).not.toBe(-1);
-    });
-});
+// describe("Lobby", () => {
+//     test("Live", async () => {
+//         await page.goto(`${url}/Lobby/Live`);
+//         const html = await page.content();
+//         const $ = cheerio.load(html.replace(/\n/g, ''));
+//         const list = $('.game-list > li');
+//         list.each((index, element) => {
+//             const target = element.attribs['game-box'];
+//             const click = element.attribs['ng-click'];
+//             expect(click).toBe(checkList.live[target]);
+//         });
+//     });
 
-describe("錯誤", () => {
-    test("404", async () => {
-        expect(errLoad).toEqual([]);
-    });
-    test("ConsoleError", async () => {
-        expect(errConsole).toEqual([]);
-    });
-});
+//     test("Sport", async () => {
+//         await page.goto(`${url}/Lobby/Sport`);
+//         const html = await page.content();
+//         const $ = cheerio.load(html.replace(/\n/g, ''));
+//         const list = $('.game-list > li');
+//         list.each((index, element) => {
+//             const target = element.attribs['game-box'];
+//             const click = element.attribs['ng-click'];
+//             expect(click).toBe(checkList.sport[target]);
+//         });
+//     });
+
+//     test("Lottery", async () => {
+//         await page.goto(`${url}/Lobby/Lottery`);
+//         const html = await page.content();
+//         const $ = cheerio.load(html.replace(/\n/g, ''));
+//         const list = $('.game-list > li');
+//         list.each((index, element) => {
+//             const target = element.attribs['game-box'];
+//             const click = element.attribs['ng-click'];
+//             expect(click).toBe(checkList.lottery[target]);
+//         });
+//     });
+// });
+
+// describe("試玩", () => {
+//     test("Logo", async () => {
+//         await page.goto(`${url}/Trial/Apply`);
+//         const logo = await page.evaluate(() => {
+//             const title = document.querySelector('#trial-logo');
+//             return getComputedStyle(title)[`background-image`];
+//         });
+//         const checkUrl = logo.search(webname);
+//         expect(checkUrl).not.toBe(-1);
+//     });
+// });
+
+// describe("錯誤", () => {
+//     test("沒有404", async () => {
+//         expect(errLoad).toEqual([]);
+//     });
+//     test("沒有ConsoleError", async () => {
+//         expect(errConsole).toEqual([]);
+//     });
+// });
 
 afterAll(() => {
     browser.close();
